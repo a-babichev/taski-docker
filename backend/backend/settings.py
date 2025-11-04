c44fbac6c4618352fd7a3cfae3782e04b7c33375
Alexey Babichev <me@a-babichev.ru>
2025-11-04 22:21:55 +0300

    fixed workflows 2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-for-dev-only')
 
 DEBUG = os.environ.get('DEBUG') == 'True'
 
